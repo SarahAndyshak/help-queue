8c65b712ee3efa7bbcdd804bf83cfb7fbb907a95
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-04-17 19:21:00 -0700

    Update project to add local state with button; new image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ticketsImage from "./../img/tickets.png";
+// import ticketsImage from "./../img/tickets.png";
+import ticketsImage2 from "./../img/tickets2.jpg";
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
-      <img src={ticketsImage} alt="An image of tickets" />
+      {/* <img src={ticketsImage} alt="An image of tickets" /> */}
+      <img src={ticketsImage2} alt="An image of tickets" />
     </React.Fragment>
   );
 }
